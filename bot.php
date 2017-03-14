0f31aa0b02c621bde5a4eb487e134496f88b8246
DESKTOP-KRGL24A\bent4 <bent45610@gmail.com>
2017-03-14 20:34:08 +0700

    no message

--- bot.php
+++ bot.php
@@ -16,7 +16,7 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
   $arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$arrJson['events'][0]['source']['userId'];
-}else if($arrJson['events'][0]['message']['text'] == "ชื่ออะไร" or $arrJson['events'][0]['message']['text'] == "อะไร"){
+}else if($arrJson['events'][0]['message']['text'] == "ชื่ออะไร" || $arrJson['events'][0]['message']['text'] == "อะไร"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
